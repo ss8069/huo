574bd43d5ee36e664006c0b710332a6e67bd4b96
ss8069 <said@totacompany.com>
2026-09-11 19:25:18 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='mc-v72';
+const C='mc-v76';
 const CORE=['./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(CORE)));self.skipWaiting()});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>clients.claim()).then(()=>clients.matchAll({type:'window'})).then(cs=>cs.forEach(c=>c.postMessage({type:'mc-update'})))));
